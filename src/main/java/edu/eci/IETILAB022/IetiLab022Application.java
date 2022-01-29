4c166d9c93f96e32e9d2b871f077a45a566d03c6
Ricardo Amaya <ricardo.amaya@escuelaing.edu.co>
2022-01-28 19:12:42 -0500

    INITIAL COMMIT

---
+++ src/main/java/edu/eci/IETILAB022/IetiLab022Application.java
@@ -0,0 +1,13 @@
+package edu.eci.IETILAB022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IetiLab022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IetiLab022Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/IETILAB022/IetiLab022ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.IETILAB022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IetiLab022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
